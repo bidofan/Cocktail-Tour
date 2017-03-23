e6aa04a502fff0c498fe28cf91185279a82d274d
trigun117 <chuvak117@gmail.com>
2017-03-24 00:37:21 +0200

    Change rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-	root 'recipes#index'
+	get '/mysterious-waters-14828.herokuapp.com' => 'recipes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
