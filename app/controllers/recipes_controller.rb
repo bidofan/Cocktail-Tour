d87433f40fc25d2631a6e77dbf5a845169c0a4b9
trigun117 <chuvak117@gmail.com>
2017-03-24 09:15:42 +0200

    Add controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,21 @@ class RecipesController < ApplicationController
 	def index
 		@recipes = Recipe.all
 	end
+
+	def show
+		@recipes = Recipe.find(params[:id])	
+	end
+
+	def new
+		@recipes = Recipe.new
+	end
+
+	def edit
+		@recipes = Recipe.find(params[:id])
+	end
+
+	private
+		def recipe_params
+			params.require(:recipes).permit(:text, :recipe)
+		end
 end
