102d31cb7c40512b3943e8e1c613651aa1d4c4f5
vasuok <sbidochka@mail.ru>
2017-04-18 19:29:18 +0300

    Addind about us

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'peersons/profile'
-
-	root 'welcome#welcome'
-	resources :recipes do
-		resources :comments
-	end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#welcome'
+  get 'about_us/about_us'
+  resources :recipes  
 end
